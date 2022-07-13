4e135966342de1807ce19972fb38b8b476ab739a
Anderson Vieira <andersonrvr@gmail.com>
2022-07-13 18:54:36 -0300

    remove:verify user

--- src/services/family/deleteFamily.service.ts
+++ src/services/family/deleteFamily.service.ts
@@ -5,12 +5,6 @@ import { AppError } from "../../errors/appError";
 const deleteFamilyService = async (id: string): Promise<string> => {
   const familyRepository = AppDataSource.getRepository(Family);
 
-  const findFamily = await familyRepository.findOneBy({ id: id });
-
-  if (!findFamily) {
-    throw new AppError("Family not found");
-  }
-
   await familyRepository.delete(id);
 
   return "Family deleted with success.";
