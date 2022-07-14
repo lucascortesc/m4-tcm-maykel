4e4b5b4b4acff09ac4e9df40fa36d0b208eca7d7
lucascortesc <ccordeiro.lucas@gmail.com>
2022-07-14 16:32:07 -0300

    fix: interface IPacient

--- src/interfaces/pacient/index.ts
+++ src/interfaces/pacient/index.ts
@@ -8,13 +8,6 @@ export interface ICreatePacient {
   is_owner: boolean;
 }
 
-export interface IPacient {
+export interface IPacient extends ICreatePacient {
   id: string;
-  cpf: string;
-  name: string;
-  last_name: string;
-  age: number;
-  tel: string;
-  family_id: string;
-  is_owner: boolean;
 }
