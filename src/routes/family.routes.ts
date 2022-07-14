b03e316e7ad0acd1d535f05f113f464f9a091d8a
ogaki <pauloogaki.principal@gmail.com>
2022-07-14 13:08:30 -0300

    feat: create list one family

--- src/routes/family.routes.ts
+++ src/routes/family.routes.ts
@@ -1,5 +1,6 @@
 import { Router } from "express";
 import deleteFamilyController from "../controllers/family/deleteFamily.controller";
+import listOneFamilyController from "../controllers/family/listOneFamily.controller"
 import { authorization } from "../middlewares/Authorization.middleware";
 import verifyFamily from "../middlewares/verifyFamily.middleware";
 
@@ -10,6 +11,12 @@ familyRoutes.delete(
   authorization,
   verifyFamily,
   deleteFamilyController
+);
+familyRoutes.get(
+  "/:id",
+  authorization,
+  verifyFamily,
+  listOneFamilyController
 );
 
 export default familyRoutes;
