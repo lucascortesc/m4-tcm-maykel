925e17616d745bd9b33ba88bf07761f3f373a8d0
ogaki <pauloogaki.principal@gmail.com>
2022-07-20 13:21:21 -0300

    feat/create get token google function

--- src/auth/google/index.ts
+++ src/auth/google/index.ts
@@ -1,15 +1,46 @@
-import * as queryString from "query-string"
+import axios from "axios";
+import * as queryString from "query-string";
 
 const queryParams = queryString.stringify({
-    client_id: process.env.GOOGLE_ID,
-    redirect_uri: "https://localhost:8080/auth/google",
-    scope: [
-        "https://www.googleapis.com/auth/userinfo.email",
-        "https://www.googleapis.com/auth/userinfo.profile"
-    ].join(" "),
-    response_type: "code",
-    access_type: "offline",
-    prompt: "consent"
-})
+  client_id: process.env.GOOGLE_ID,
+  redirect_uri: "http://localhost:3000/auth/google",
+  scope: [
+    "https://www.googleapis.com/auth/userinfo.email",
+    "https://www.googleapis.com/auth/userinfo.profile",
+  ].join(" "),
+  response_type: "code",
+  access_type: "offline",
+  prompt: "consent",
+});
 
-const loginGoogleUrl = `https://accounts.google.com/o/oauth2/v2/auth?${queryParams}`
+export const loginGoogleUrl = `https://accounts.google.com/o/oauth2/v2/auth?${queryParams}`;
+
+export async function getGoogleToken(code: any) {
+  const { data } = await axios({
+    url: "https://oauth2.googleapis.com/token",
+    method: "post",
+    data: {
+      client_id: process.env.GOOGLE_ID,
+      client_secret: process.env.GOOGLE_SECRET,
+      redirect_uri: "http://localhost:3000/auth/google",
+      grant_type: "authorization_code",
+      code,
+    },
+  });
+
+  return data.access_token;
+}
+
+export async function getGoogleInfo(token: any) {
+  const { data } = await axios({
+    url: "https://www.googleapis.com/oauth2/v2/userinfo",
+    method: "get",
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+
+  console.log(data);
+
+  return data;
+}
--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -1,7 +1,20 @@
 import { Request, Response, Router } from "express";
+import { getGoogleInfo, getGoogleToken, loginGoogleUrl } from "../auth/google";
 
 const authRoutes = Router()
 
-authRoutes.get("/auth/google", (req: Request, res: Response) => {
-    
+authRoutes.get("/login/google", (req: Request, res: Response) => {
+    res.redirect(loginGoogleUrl)
 })
+
+authRoutes.get("/auth/google", async (req: Request, res: Response) => {
+    const { code } = req.query;
+  
+    const token = await getGoogleToken(code);
+    const data = await getGoogleInfo(token);
+  
+    res.json(data);
+  });
+  
+
+export default authRoutes
